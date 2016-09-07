05824af42130ca2a1e576a5264f1e07a5c9c65fe
edvardian <kainoss@hotmail.com>
2016-09-07 01:51:12 -0700

    Added player icon
    Added player icon

---
+++ page.js
@@ -0,0 +1,4 @@
+
+//EVENT HANDLERS
+document.getElementById("gameTitle").addEventListener("mouseover", function(){document.getElementById("gameTitle").innerHTML = document.getElementById("nameChange").value });
+document.getElementById("gameTitle").addEventListener("mouseout", function(){document.getElementById("gameTitle").innerHTML = "CHARGEBLADE"});

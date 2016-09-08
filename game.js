1b074a034d36ed7851ba7823c5a673bff8a7ffe8
edvardian <kainoss@hotmail.com>
2016-09-07 23:52:02 -0700

    Adding interactivity to player sprite
    Streamlining files and adding interactivity to game state.

--- game.js
+++ game.js
@@ -1,17 +1,44 @@
-var game = new Phaser.Game( 500, 300, Phaser.AUTO, "gameStage" );
+var game = new Phaser.Game( 720, 480, Phaser.AUTO, "gameStage" );
 
 var gameState = {
-    preload: function(){
-        game.load.image("player", "assets/images/player.png");
-    },
-    
-    create: function(){
-        game.add.sprite(250, 100, "player");
-    },
-    
-    update: function(){
-        
-    }
+	preload: function(){
+		game.load.image("player", "assets/images/player.png");
+	},
+
+	create: function(){
+		stageCenterX = (game.width/2);
+		stageCenterY = (game.height/2);
+		player = this.add.sprite(stageCenterX, stageCenterY, "player");
+		player.anchor.set(0.5);
+
+		game.physics.arcade.enable(player);
+
+		player.body.drag.set(70);
+		player.body.maxVelocity.set(200);
+		cursors = this.input.keyboard.createCursorKeys();
+
+		},
+
+	update: function(){
+		if (cursors.up.isDown){
+			game.physics.arcade.accelerationFromRotation(player.rotation, 300, player.body.acceleration);
+		}
+		else{
+			player.body.acceleration.set(0);
+		}
+
+		if (cursors.left.isDown){
+			player.body.angularVelocity = -300;
+		}
+		else if (cursors.right.isDown){
+			player.body.angularVelocity = 300;
+		}
+		else{
+			player.body.angularVelocity = 0;
+		}
+
+		game.world.wrap(player, 16);
+	}
 }
 
 game.state.add("gameState", gameState);

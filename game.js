5ef6b5409d381ff935ac7f16c43a55a793171358
Eddie Yang <kainoss@hotmail.com>
2016-09-08 04:22:11 +0000

    Adding phaser game startup

--- game.js
+++ game.js
@@ -1,1 +1,18 @@
-var game = null
+var game = new Phaser.Game( 500, 300, Phaser.AUTO, "gameStage" );
+
+var gameState = {
+    preload: function(){
+        game.load.image("player", "assets/images/player.png");
+    },
+    
+    create: function(){
+        game.add.sprite(250, 100, "player");
+    },
+    
+    update: function(){
+        
+    }
+}
+
+game.state.add("gameState", gameState);
+game.state.start("gameState");

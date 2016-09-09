3a3df35c6477e6b4a9627923983c44d37338aac2
edvardian <kainoss@hotmail.com>
2016-09-09 02:27:15 -0700

    Added mousetracking and movement
    Updated game.js with new functions that allows the player sprite to
    track the mouse, and move with the WASD keys.

--- game.js
+++ game.js
@@ -1,4 +1,5 @@
 var game = new Phaser.Game( 720, 480, Phaser.AUTO, "gameStage" );
+var maxVelocity = 100;
 
 var gameState = {
 	preload: function(){
@@ -6,40 +7,55 @@ var gameState = {
 	},
 
 	create: function(){
-		stageCenterX = (game.width/2);
-		stageCenterY = (game.height/2);
-		player = this.add.sprite(stageCenterX, stageCenterY, "player");
+		player = this.add.sprite(game.world.centerX, game.world.centerY, "player");
 		player.anchor.set(0.5);
 
 		game.physics.arcade.enable(player);
 
-		player.body.drag.set(70);
-		player.body.maxVelocity.set(200);
+		player.body.drag.set(1000);
+		player.body.maxVelocity.set(1);
 		cursors = this.input.keyboard.createCursorKeys();
+		mousePoint = this.input.mousePointer;
 
 		},
 
 	update: function(){
-		if (cursors.up.isDown){
-			game.physics.arcade.accelerationFromRotation(player.rotation, 300, player.body.acceleration);
+		//New input methods. Has Sprite looking pointing to the mouse, and moves with WASD keys.
+
+		//player.angle = playerRotation(player.position, mousePoint.position);
+		player.rotation = game.physics.arcade.angleToPointer(player.body, mousePoint);
+
+		if (this.input.keyboard.isDown(Phaser.KeyCode.W)){
+			game.physics.arcade.accelerateToXY(player, player.x, (player.y - 1), 1000, maxVelocity, maxVelocity );
+			console.log("W is pressed");
+		}
+		else if (this.input.keyboard.isDown(Phaser.KeyCode.S)){
+			game.physics.arcade.accelerateToXY(player, player.x, (player.y + 1), 1000, maxVelocity, maxVelocity );
+			console.log("S is pressed");
+		}
+		else if (this.input.keyboard.isDown(Phaser.KeyCode.A)){
+			game.physics.arcade.accelerateToXY(player, (player.x - 1), player.y, 1000, maxVelocity, maxVelocity );
+			console.log("A is pressed");
+		}
+		else if (this.input.keyboard.isDown(Phaser.KeyCode.D)){
+			game.physics.arcade.accelerateToXY(player, (player.x + 1), player.y, 1000, maxVelocity, maxVelocity );
+			console.log("D is pressed");
 		}
 		else{
 			player.body.acceleration.set(0);
 		}
 
-		if (cursors.left.isDown){
-			player.body.angularVelocity = -300;
-		}
-		else if (cursors.right.isDown){
-			player.body.angularVelocity = 300;
-		}
-		else{
-			player.body.angularVelocity = 0;
-		}
-
 		game.world.wrap(player, 16);
+
 	}
 }
+
+/*
+function playerRotation(playerPosition, mousePosition){
+	var angle = Math.atan2( (mousePosition.y - playerPosition.y), (mousePosition.x - playerPosition.x) ) *  180 / Math.PI ;
+	return angle;
+}
+*/
 
 game.state.add("gameState", gameState);
 game.state.start("gameState");
